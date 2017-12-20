2dce892fc1d16e349b3f9fb8ea0e3ad6eb0e0b36
Alejandro Piedra <apiedra@octi088.gmalv.grupomutual.com>
2017-12-20 11:09:44 -0600

    ClaseFinal

---
+++ 2/MyFinalApp/app/src/main/java/testappsample/curso/com/myfinalapp/Main2Activity.java
@@ -0,0 +1,29 @@
+package testappsample.curso.com.myfinalapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private TextView message, year;
+    Intent intentParams;
+    String customMessage;
+    int customYear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        intentParams = getIntent();
+        customMessage = intentParams.getStringExtra("customMessage");
+        customYear = intentParams.getIntExtra("currentYear",0);
+
+        message = (TextView) findViewById(R.id.mymessage);
+        year = (TextView) findViewById(R.id.year);
+
+        message.setText(customMessage+" "+customYear);
+    }
+}
